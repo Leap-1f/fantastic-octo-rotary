1f959c9138b41dd1d42f8420658d52322c886bbc
Danzkreig <festeringwound@proton.me>
2024-04-10 18:46:57 +0800

    index created and dotenv created.

---
+++ be/index.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import cloudinary from "cloudinary";
+import bcrypt from "bcrypt";
+dotenv.config();
+const port = 8080;
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+// dotenv configuration.
+let {
+  DBUSER,
+  DBPASS,
+  CLOUDINARYSECRET,
+  CLOUDINARYKEY,
+  CLOUDINARYUSER,
+  EMAILUSER,
+  EMAILPASS,
+  EMAILTOKEN,
+} = process.env;
+cloudinary.config({
+  cloud_name: CLOUDINARYUSER,
+  api_key: CLOUDINARYKEY,
+  api_secret: CLOUDINARYSECRET,
+});
+var transport = nodemailer.createTransport({
+  service: "Mail.ru",
+  auth: {
+    user: EMAILUSER,
+    pass: EMAILTOKEN,
+    auth: EMAILPASS,
+  },
+});
+const MONGO_CONNECTION_STRING = `mongodb+srv://${DBUSER}:${DBPASS}@foodapp.pk3ugl6.mongodb.net/`;
+
+mongoose
+  .connect(MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB: ", err);
+  });
+// mongodb connection
+
+cloudinary.api.resources(function (error, result) {
+  if (error) {
+    console.error("Error retrieving Cloudinary resources:", error);
+  } else {
+    const imageUrls = result.resources.map((resource) => resource.secure_url);
+  }
+});
+
+//cloudinary connection
+
+const saltRounds = 10;
+
+// Initialize the app
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+// app listening
+app.listen(port, () => {
+  console.log(`Your server is on on the port "http:localhost:${8080}"`);
+});
