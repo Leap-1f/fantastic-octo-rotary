a15cba45e560bfe71832f7a0708489beb88b545c
Bayr-Erdene Enkhtur <23LP1331@G401-CC0101.local>
2024-04-11 21:36:00 +0800

    add dashboard

---
+++ fe/src/admin-component/adminIncome.tsx
@@ -0,0 +1,3 @@
+export const AdminIncome = () => {
+  return <div></div>;
+};
--- fe/src/admin-component/header.tsx
+++ fe/src/admin-component/header.tsx
@@ -1,5 +1,21 @@
 "use client";
-import { Box, Typography } from "@mui/material";
-export function Header() {
-  return <div>sadasdsadsa</div>;
+import { FaRegBell } from "react-icons/fa6";
+import { FaRegUser } from "react-icons/fa6";
+export function AdminHeader() {
+  return (
+    <div className="flex justify-between bg-black h-[5%] w-full">
+      <div className="text-white flex justify-center items-center ml-[10%]">
+        Pinecone
+      </div>
+      <div className="flex justify-center items-center *:text-white gap-[30%] mr-[10%] *:cursor-pointer">
+        <div>
+          <FaRegBell className="h-6 w-6" />
+        </div>
+        <div>
+          <FaRegUser className="h-6 w-6" />
+        </div>
+        <div>Username</div>
+      </div>
+    </div>
+  );
 }
