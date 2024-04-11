9a6a5a39b7baff5cf34b5f41ddb03b8b635f3eeb
Danzkreig <festeringwound@proton.me>
2024-04-11 17:26:21 +0800

    asdf

--- fe/app/admin/login/page.tsx
+++ fe/app/admin/login/page.tsx
@@ -2,21 +2,75 @@
 import { TextField, Button, Box, Stack, Typography } from "@mui/material";
 export default function Page() {
   return (
-    <Stack
-      spacing={4}
+    <Box
       sx={{
-        width: "20vw",
+        width: "100vw",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        height: "100vh",
       }}
-      direction="column"
     >
-      <Typography variant="h4">Бүртгүүлэх</Typography>
-      <TextField label="Name" variant="outlined"></TextField>
-      <Button variant="contained" size="large">
-        Submit
-      </Button>
-    </Stack>
+      <Stack
+        spacing={4}
+        direction="column"
+        width={{
+          width: "19%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "1%",
+          borderRadius: "10px",
+          border: "1px solid #ccc",
+        }}
+      >
+        <Typography sx={{ fontWeight: "Bold", fontSize: "32px", pt: "1vh" }}>
+          Бүртгүүлэх
+        </Typography>
+        <Stack direction="column" spacing={3} sx={{ width: "100%" }}>
+          <Stack direction="column" spacing={1}>
+            <Typography sx={{ fontSize: "16px", fontWeight: "300" }}>
+              Таны имэйл
+            </Typography>
+            <TextField
+              placeholder="Имэйл"
+              variant="outlined"
+              sx={{ width: "100%", bgcolor: "#F7F7F8" }}
+            />
+          </Stack>
+          <Stack direction="column" spacing={1} sx={{ width: "100%" }}>
+            <Typography sx={{ fontSize: "16px", fontWeight: "300" }}>
+              Таны нэр
+            </Typography>
+            <TextField
+              placeholder="Нэр"
+              variant="outlined"
+              sx={{ width: "100%", bgcolor: "#F7F7F8" }}
+            />
+          </Stack>
+
+          <Button
+            variant="contained"
+            size="large"
+            sx={{
+              bgcolor: "black",
+              color: "white",
+              width: "100%",
+              transition: "all 0.3s",
+              mb: "1vh",
+              borderBottom: "1px solid #ccc",
+              ":hover": {
+                transform: "scale(1.05)",
+                color: "white",
+                width: "100%",
+                bgcolor: "black",
+              },
+            }}
+          >
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
