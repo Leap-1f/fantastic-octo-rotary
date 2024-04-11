c1bd71a6fa8963331de08ed1653bab42c171d5fd
Danzkreig <festeringwound@proton.me>
2024-04-11 16:58:18 +0800

    asdf

---
+++ fe/app/admin/login/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { TextField, Button, Box, Stack, Typography } from "@mui/material";
+export default function Page() {
+  return (
+    <Stack
+      spacing={4}
+      sx={{
+        width: "20vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      direction="column"
+    >
+      <Typography variant="h4">Бүртгүүлэх</Typography>
+      <TextField label="Name" variant="outlined"></TextField>
+      <Button
+        variant="contained"
+        sx={{widtg, {}}}
+        size="large"
+      >
+        Submit
+      </Button>
+    </Stack>
+  );
+}
