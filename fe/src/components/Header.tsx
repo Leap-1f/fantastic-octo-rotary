13d2f86ab3a44bf0d16b8c3cbd131398a3e1ab1c
dorjsambuu amarmend <23LP7975@aiRo.local>
2024-04-11 20:52:55 -0700

    first commit

---
+++ fe/src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <>
+      <div className="flex  items-center">
+        <div className="text-2xl font-bold"></div>
+        <div className="">
+          <p className="text-purple-500">gg</p>
+          <p className="text-purple-500">ngagagag</p>
+          <p className="text-purple-500">ngagagag</p>
+          <p className="bg-purple-500">sadasdas</p>
+        </div>
+      </div>
+    </>
+  );
+}
